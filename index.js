ebaff7c0d4a17dc4b5e9a8ef43fb7770fe5fd2df
Chris Dickinson <christopher.s.dickinson@gmail.com>
2013-02-01 12:43:38 -0800

    add live support, clean up code a bit, add help message

--- index.js
+++ index.js
@@ -2,70 +2,88 @@ var http = require('http')
   , spawn = require('child_process').spawn
   , filed = require('filed')
   , oppressor = require('oppressor')
-  , Path = require('path')
+  , path = require('path')
   , URL = require('url')
   , response_stream = require('response-stream')
   , fs = require('fs')
-  , fake_index_html = fs.readFileSync(Path.join(__dirname, 'fake_index.html'), 'utf8')
+  , fake_index_html = fs.readFileSync(path.join(__dirname, 'fake_index.html'), 'utf8')
   , process = require('process')
   , console = require('console')
   , optimist = require('optimist').argv
   , LiveReloadServer = require('live-reload')
+  , help = require('./help')
 
 var argv = process.argv.slice(/node/.test(process.argv[0]) ? 2 : 1)
   , browserify_path = which_browserify()
   , browserify_args
   , ENTRY_POINT
+  , LIVE_PORT
   , PORT
 
-get_args()
+if(!get_args()) {
+  process.exit(1)
+}
+
+console.log('using '+browserify_path.replace(process.cwd(), '.'))
 
 http.createServer(function(req, resp) {
   var url = URL.parse(req.url).pathname.slice(1) || 'index.html'
-    , path = Path.resolve(Path.join(process.cwd(), url))
+    , filepath = path.resolve(path.join(process.cwd(), url))
     , stream
     , b
 
-  if(path === ENTRY_POINT) {
+  if(filepath === ENTRY_POINT) {
     console.log('/'+url, browserify_path+' '+browserify_args.join(' '))
     stream = response_stream((b = spawn(browserify_path, browserify_args)).stdout)
 
     b.stderr.pipe(process.stdout)
   } else {
     console.log('/'+url)
-    if(!fs.existsSync(path)) {
+    if(!fs.existsSync(filepath)) {
       return fake_index(resp)
     } else {
-      stream = filed(path)
+      stream = filed(filepath)
     }
   }
 
-  stream.on('error', function() { console.log('whaaaat'); resp.end('500') })
+  stream.on('error', function(err) {
+    console.log('500 - '+url.pathname)
+    if(err && err.stack) {
+      console.log('\t'+err.stack.split('\n').join('\n\t'))
+    }
+    resp.end('500')
+  })
 
   stream.pipe(resp)
 }).listen(PORT)
 
-if (optimist.live) {
-  var liveReloadPort = optimist.live === true ? 9967 :
-    optimist.live
+if(optimist.live) {
+  LIVE_PORT = optimist.live === true ? 9967 : optimist.live
 
   LiveReloadServer({
-    port: liveReloadPort
+    port: LIVE_PORT 
   })
 }
 
 function fake_index(resp) {
-  resp.writeHead(200, {'content-type':'text/html'})
-  var liveReloadText = optimist.live ?
-    "<script src='http://localhost:" + liveReloadPort + "'></script>" : ""
+  var live_text
+    , html
 
-  var html = fake_index_html.
+  resp.writeHead(200, {'content-type':'text/html'})
+
+  live_text = '<script src="http://localhost:"' + LIVE_PORT + '"></script>'
+  html = fake_index_html.
     replace('{{ PATH }}', ENTRY_POINT.replace(process.cwd(), '')).
-    replace('{{ EXTRA }}', liveReloadText)
+    replace('{{ EXTRA }}', LIVE_PORT ? live_text : '')
+
   resp.end(html)
 }
 
 function get_args() {
+  if(!argv.length) {
+    return help()
+  }
+
   for(var i = 0, len = argv.length; i < len; ++i) {
     if(argv[i] === '--') {
       break
@@ -74,26 +92,26 @@ function get_args() {
 
   browserify_args = argv.splice(i+1, argv.length - i)
 
-  ENTRY_POINT = Path.resolve(
-    Path.join(process.cwd(), argv[0] || 'main.js')
+  ENTRY_POINT = path.resolve(
+    path.join(process.cwd(), argv[0] || 'main.js')
   )
 
   PORT = +argv[1] || 9966
   console.log('listening on '+PORT)
 
   browserify_args.unshift(ENTRY_POINT)
+
+  return true
 }
 
 function which_browserify() {
-  if (optimist.browserify) {
+  if(optimist.browserify) {
     return optimist.browserify
   }
 
-  var local = Path.join(process.cwd(), 'node_modules/.bin/browserify')
+  var local = path.join(process.cwd(), 'node_modules/.bin/browserify')
   if(fs.existsSync(local)) {
-    console.log('using browserify@'+local.replace(process.cwd(), '.'))
     return local
   }
-  console.log('using global browserify')
   return 'browserify'
 }
