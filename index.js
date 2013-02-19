dadc2f8f664b33dcbae6ca08791d2b304d528280
Raynos <raynos2@gmail.com>
2013-02-18 16:51:55 -0800

    support things

--- index.js
+++ index.js
@@ -2,54 +2,99 @@ var http = require('http')
   , spawn = require('child_process').spawn
   , filed = require('filed')
   , oppressor = require('oppressor')
-  , Path = require('path')
+  , path = require('path')
   , URL = require('url')
   , response_stream = require('response-stream')
   , fs = require('fs')
-  , fake_index_html = fs.readFileSync(Path.join(__dirname, 'fake_index.html'), 'utf8')
+  , fake_index_html = fs.readFileSync(path.join(__dirname, 'fake_index.html'), 'utf8')
+  , process = require('process')
+  , console = require('console')
+  , optimist = require('optimist').argv
+  , LiveReloadServer = require('live-reload')
+  , help = require('./help')
 
 var argv = process.argv.slice(/node/.test(process.argv[0]) ? 2 : 1)
   , browserify_path = which_browserify()
   , browserify_args
   , ENTRY_POINT
+  , LIVE_PORT
   , PORT
 
-fix_filed()
-get_args()
+if(!get_args()) {
+  process.exit(1)
+}
+
+console.log('using '+browserify_path.replace(process.cwd(), '.'))
 
 http.createServer(function(req, resp) {
   var url = URL.parse(req.url).pathname.slice(1) || 'index.html'
-    , path = Path.resolve(Path.join(process.cwd(), url))
+    , filepath = path.resolve(path.join(process.cwd(), url))
     , stream
     , b
+    , uri = URL.parse(req.url, true)
 
-  if(path === ENTRY_POINT) {
-    console.log('/'+url, 'browserify '+browserify_args.join(' '))
-    stream = response_stream((b = spawn(browserify_path, browserify_args)).stdout)
-
-    resp.setHeader("content-type", "application/javascript")
+  if(filepath === ENTRY_POINT || uri.query.browserify === "true") {
+    console.log('/'+url, browserify_path+' '+browserify_args.join(' '))
+    var args = browserify_args.slice()
+    args[0] = filepath
+    stream = response_stream((b = spawn(browserify_path, args)).stdout)
 
     b.stderr.pipe(process.stdout)
   } else {
     console.log('/'+url)
-    if(!fs.existsSync(path)) {
-      return fake_index(resp)
+    if(!fs.existsSync(filepath)) {
+      return fake_index(req, resp)
     } else {
-      stream = filed(path)
+      stream = filed(filepath)
     }
   }
 
-  stream.on('error', function() { console.log('whaaaat'); resp.end('500') })
+  stream.on('error', function(err) {
+    console.log('500 - '+url.pathname)
+    if(err && err.stack) {
+      console.log('\t'+err.stack.split('\n').join('\n\t'))
+    }
+    resp.end('500')
+  })
 
   stream.pipe(resp)
 }).listen(PORT)
 
-function fake_index(resp) {
+if(optimist.live) {
+  LIVE_PORT = optimist.live === true ? 9967 : optimist.live
+
+  LiveReloadServer({
+    port: LIVE_PORT
+  })
+}
+
+function fake_index(req, resp) {
+  var live_text
+    , html
+
   resp.writeHead(200, {'content-type':'text/html'})
-  resp.end(fake_index_html.replace('{{ PATH }}', ENTRY_POINT.replace(process.cwd(), '')))
+
+  var uri = URL.parse(req.url, true)
+
+  var indexPath = uri.query.p || ENTRY_POINT.replace(process.cwd(), "")
+
+  if (uri.query.p) {
+    indexPath += "?browserify=true"
+  }
+
+  live_text = '<script src="http://localhost:' + LIVE_PORT + '"></script>'
+  html = fake_index_html.
+    replace('{{ PATH }}', indexPath).
+    replace('{{ EXTRA }}', LIVE_PORT ? live_text : '')
+
+  resp.end(html)
 }
 
 function get_args() {
+  if(!argv.length || optimist.h || optimist.help) {
+    return help()
+  }
+
   for(var i = 0, len = argv.length; i < len; ++i) {
     if(argv[i] === '--') {
       break
@@ -58,31 +103,26 @@ function get_args() {
 
   browserify_args = argv.splice(i+1, argv.length - i)
 
-  ENTRY_POINT = Path.resolve(
-    Path.join(process.cwd(), argv[0] || 'main.js')
+  ENTRY_POINT = path.resolve(
+    path.join(process.cwd(), argv[0] || 'main.js')
   )
 
   PORT = +argv[1] || 9966
   console.log('listening on '+PORT)
 
   browserify_args.unshift(ENTRY_POINT)
-}
 
-function fix_filed() {
-  // bugfix (mikael's filed doesn't return `dst` on `pipe(dst)`, SIGH):
-  var old_pipe = filed.File.prototype.pipe
-  filed.File.prototype.pipe = function(x, y) {
-    old_pipe.call(this, x, y)
-    return x
-  }
+  return true
 }
 
 function which_browserify() {
-  var local = Path.join(process.cwd(), 'node_modules/.bin/browserify')
+  if(optimist.browserify) {
+    return optimist.browserify
+  }
+
+  var local = path.join(process.cwd(), 'node_modules/.bin/browserify')
   if(fs.existsSync(local)) {
-    console.log('using browserify@'+local.replace(process.cwd(), '.'))
     return local
   }
-  console.log('using global browserify')
   return 'browserify'
 }
